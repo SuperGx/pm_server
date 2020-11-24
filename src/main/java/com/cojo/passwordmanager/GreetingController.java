f6d2c1dc72f5e26db7622624ab433a8d6cc3c54d
SuperGx <mitreageorge14@stud.ase.ro>
2020-11-24 20:41:03 +0200

    todo

--- src/main/java/com/cojo/passwordmanager/GreetingController.java
+++ src/main/java/com/cojo/passwordmanager/GreetingController.java
@@ -18,4 +18,9 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	// TODO 
+	// primeasca un GET pentru credentialele lui (returneaza byte[] cu datele criptate)
+	// autentificare 
+
 }
